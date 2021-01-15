49eb8b0182c995dd319f167ad6afeaff51c3fa48
Atep Saprudin <atep.saprudin86@gmail.com>
2021-01-15 16:39:50 +0700

    Update 15-01-2021

---
+++ application/views/page/home2.php
@@ -0,0 +1,56 @@
+    <section class="tm-welcome-section" style="background-color: #bbbfca;">
+      <div class="container tm-position-relative">
+<!--         <div class="tm-lights-container">
+          <img src="img/light.png" alt="Light" class="light light-1">
+          <img src="img/light.png" alt="Light" class="light light-2">
+          <img src="img/light.png" alt="Light" class="light light-3">  
+        </div> -->        
+        <div class="row tm-welcome-content">
+          <h2 class="black-text tm-handwriting-font tm-welcome-header">Selamat Datang</h2>
+          <h2 class="black-text tm-welcome-header-1">Payroll Management PT.Indoneptune Net</h2>   
+        </div>
+        <img src="<?= base_url().'assets/' ?>images/table-set.png" alt="Table Set" class="tm-table-set img-responsive">             
+      </div>      
+    </section>
+    <div class="tm-main-section light-gray-bg">
+      <div class="container" id="main">        
+        <section class="tm-section tm-section-margin-bottom-0 row">
+          <div class="col-lg-12 tm-section-header-container">
+            <h2 class="tm-section-header gold-text tm-handwriting-font">Our Menu</h2>
+            <div class="tm-hr-container"><hr class="tm-hr"></div>
+          </div>
+          <div class="col-lg-12 tm-popular-items-container">
+            <div class="tm-popular-item">
+              <img src="<?= base_url().'assets/' ?>images/popular-1.jpg" alt="Popular" class="tm-popular-item-img">
+              <div class="tm-popular-item-description">
+                <h3 class="tm-handwriting-font tm-popular-item-title"><span class="tm-handwriting-font bigger-first-letter">P</span>erson</h3><hr class="tm-popular-item-hr">
+                <p>Data Bagian,Personil,Master Gaji,Pengajuan Cuti,Validasi Data</p>
+                <div class="order-now-container">
+                  <a href="#" class="order-now-link tm-handwriting-font">Masuk</a>
+                </div>
+              </div>              
+            </div>
+            <div class="tm-popular-item">
+              <img src="<?= base_url().'assets/' ?>images/popular-2.jpg" alt="Popular" class="tm-popular-item-img">
+              <div class="tm-popular-item-description">
+                <h3 class="tm-handwriting-font tm-popular-item-title"><span class="tm-handwriting-font bigger-first-letter">P</span>ayroll</h3><hr class="tm-popular-item-hr">
+                <p>Gaji personil</p>
+                <div class="order-now-container">
+                  <a href="#" class="order-now-link tm-handwriting-font">Masuk</a>
+                </div>
+              </div>              
+            </div>
+            <div class="tm-popular-item">
+              <img src="<?= base_url().'assets/' ?>images/popular-3.jpg" alt="Popular" class="tm-popular-item-img">
+              <div class="tm-popular-item-description">
+                <h3 class="tm-handwriting-font tm-popular-item-title"><span class="tm-handwriting-font bigger-first-letter">R</span>eport</h3><hr class="tm-popular-item-hr">
+                <p>Laporan data gaji untuk karyawan</p>
+                <div class="order-now-container">
+                  <a href="#" class="order-now-link tm-handwriting-font">Masuk</a>
+                </div>
+              </div>              
+            </div>
+          </div>          
+        </section>
+      </div>
+    </div> 
